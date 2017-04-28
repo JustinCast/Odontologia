d8de0fc0b6e6730446e85181a196f02949a6c500
bryan-ruiz <bryanrb95@gmail.com>
2017-04-28 16:23:16 -0600

    mismo
    me salio una carpeta extra de la nada referenciando a enfermedad.java

--- app/src/main/java/odontologia/proyectoodontologia/enfermedad.java
+++ app/src/main/java/odontologia/proyectoodontologia/enfermedad.java
@@ -4,5 +4,46 @@ package odontologia.proyectoodontologia;
  * Created by Bryan on 7/4/2017.
  */
 
-public class enfermedad {
+public class Enfermedad {
+    private String Enfermedad, descripcion, tratamiento;
+    private int idEnfermedad;
+
+    public Enfermedad(String enfermedad, String descripcion, String tratamiento, int idEnfermedad) {
+        Enfermedad = enfermedad;
+        this.descripcion = descripcion;
+        this.tratamiento = tratamiento;
+        this.idEnfermedad = idEnfermedad;
+    }
+
+    public String getEnfermedad() {
+        return Enfermedad;
+    }
+
+    public void setEnfermedad(String enfermedad) {
+        Enfermedad = enfermedad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public int getIdEnfermedad() {
+        return idEnfermedad;
+    }
+
+    public void setIdEnfermedad(int idEnfermedad) {
+        this.idEnfermedad = idEnfermedad;
+    }
 }
