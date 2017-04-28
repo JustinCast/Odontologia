c58715870e22a0ba006734c3f9524e0643611a1e
Justin Castillo <justincastillovalladares@gmail.com>
2017-04-28 17:05:57 -0600

    ..

--- app/src/main/java/odontologia/proyectoodontologia/Enfermedad.java
+++ app/src/main/java/odontologia/proyectoodontologia/Enfermedad.java
@@ -1,8 +1,45 @@
 package odontologia.proyectoodontologia;
 
-/**
- * Created by Justin Cast on 28/4/2017.
- */
-
 public class Enfermedad {
+    private String Enfermedad, descripcion, tratamiento;
+    private int idEnfermedad;
+
+    public Enfermedad(String enfermedad, String descripcion, String tratamiento, int idEnfermedad) {
+        Enfermedad = enfermedad;
+        this.descripcion = descripcion;
+        this.tratamiento = tratamiento;
+        this.idEnfermedad = idEnfermedad;
+    }
+
+    public String getEnfermedad() {
+        return Enfermedad;
+    }
+
+    public void setEnfermedad(String enfermedad) {
+        Enfermedad = enfermedad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public int getIdEnfermedad() {
+        return idEnfermedad;
+    }
+
+    public void setIdEnfermedad(int idEnfermedad) {
+        this.idEnfermedad = idEnfermedad;
+    }
 }
